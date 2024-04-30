0da7cd515b4d9a26ef4091223a1f92fb378640de
Raisa <volovik.raisa.kam@gmail.com>
2024-04-30 22:09:43 +0300

    add endpoints add list students

--- Student_minimal_api/Student_minimal_api/Program.cs
+++ Student_minimal_api/Student_minimal_api/Program.cs
@@ -6,29 +6,49 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-app.UseHttpsRedirection();
+//app.UseHttpsRedirection();
+var students = new List<StudentModel>();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+app.MapGet("/students", () => students);
+app.MapGet("/students/{id}", (int id) => students.FirstOrDefault(s => s.Id == id));
+app.MapPost("/students", (StudentModel student) => {
+    // Generate a unique ID for the new student
+    student.Id = students.Count > 0 ? students.Max(s => s.Id) + 1 : 1;
+    students.Add(student);
+    return Results.Created($"/students/{student.Id}", student);
+});
+app.MapPut("/students/{id}", (int id, StudentModel updatedStudent) => {
+    var existingStudent = students.FirstOrDefault(s => s.Id == id);
+    if (existingStudent == null)
+    {
+        throw new Exception("Student not found");
+    }
+    // Update the existing student with the new data
+    existingStudent.Team = updatedStudent.Team;
+    existingStudent.TableNumber = updatedStudent.TableNumber;
+    existingStudent.LastName = updatedStudent.LastName;
+    existingStudent.FirstName = updatedStudent.FirstName;
+    existingStudent.SecondName = updatedStudent.SecondName;
+    return Results.Ok();
+});
+app.MapDelete("/students/{id}", (int id) => {
+    var existingStudent = students.FirstOrDefault(s => s.Id == id);
+    if (existingStudent == null)
+    {
+        throw new Exception("Student not found");
+    }
+    students.Remove(existingStudent);
+    return Results.Ok();
 });
 
 app.Run();
 
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+public class StudentModel
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    public int Id { get; set; }
+    public int Team { get; set; }
+    public int TableNumber { get; set; }
+    public string LastName { get; set; }
+    public string FirstName { get; set; }
+    public string SecondName { get; set; }
 }
